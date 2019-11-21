caf7ef6ff998938494bacceb2a38fcc2732c38bc
DanilaOl <58003670+DanilaOl@users.noreply.github.com>
2019-11-21 09:12:10 +0300

    Add files via upload

--- Game.py
+++ Game.py
@@ -1,3 +1,4 @@
+
 import pygame
 
 pygame.init()
@@ -5,19 +6,39 @@ win = pygame.display.set_mode((1280, 720))
 
 pygame.display.set_caption('Untitled Game 1')
 
+white = (255, 255, 255)
+black = (0, 0, 0)
+
 x = 50
 y = 50
 width = 40
 height = 60
 speed = 5
 
+img = pygame.image.load('IMG_0470.gif')
+
 run = True
 while run:
-    pygame.time.delay(100)
+    pygame.time.delay(16)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    pygame.draw.rect(win, (255, 255, 255), (x, y, width, height))
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                run = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+        x -= speed
+    if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+        x += speed
+    if keys[pygame.K_UP] or keys[pygame.K_w]:
+        y -= speed
+    if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+        y += speed
+    win.fill(black)
+    pygame.draw.rect(win, white, (x, y, width, height))
     pygame.display.update()
 pygame.quit()
+
