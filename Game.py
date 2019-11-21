a6ece3e2ae96051a59fea4284d1d7cf0be4c269d
DanilaOl <58003670+DanilaOl@users.noreply.github.com>
2019-11-21 15:47:02 +0300

    Add files via upload

--- Game.py
+++ Game.py
@@ -1,30 +1,47 @@
-
 import pygame
 
 pygame.init()
 win = pygame.display.set_mode((1280, 720))
-Game_Name = 'Untitled Game 1'
+Game_Name = 'Ванильный питон'
 
 pygame.display.set_caption(Game_Name)
 
 white = (255, 255, 255)
 black = (0, 0, 0)
 
+background = pygame.image.load('Background.png')
+hero = pygame.image.load('Hero — копия.png')
 
-width = 40
-height = 60
+clock = pygame.time.Clock()
+
+width = 35
+height = 114
 speed = 5
 x = 50
-y = 720-height
+y = 720-height-5
 
 isJump = False
 jumpCount = 10
 
-img = pygame.image.load('IMG_0470.gif')
+left = False
+right = False
+animCount = 0
+
+# img = pygame.image.load('IMG_0470.gif')
+
+
+def draw_window():
+    # win.fill(white)
+    # global animCount
+    win.blit(background, (0, 0))
+    win.blit(hero, (x, y))
+
+    pygame.display.update()
+
 
 run = True
 while run:
-    pygame.time.delay(16)
+    clock.tick(60)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -34,16 +51,20 @@ while run:
                 run = False
 
     keys = pygame.key.get_pressed()
-    if (keys[pygame.K_LEFT] and x > 0) or (keys[pygame.K_a] and x > 0):
+    if (keys[pygame.K_LEFT] and x > 5) or (keys[pygame.K_a] and x > 5):
         x -= speed
-    if (keys[pygame.K_RIGHT] and x < (1280-width)) or (keys[pygame.K_d] and x < (1280-width)):
+        left = True
+        right = False
+    if (keys[pygame.K_RIGHT] and x < (1280-width-5)) or (keys[pygame.K_d] and x < (1280-width-5)):
         x += speed
-    if not(isJump):
+        left = False
+        right = True
+    if not isJump:
         # if (keys[pygame.K_UP] and y > 0) or (keys[pygame.K_w] and y > 0):
         #     y -= speed
         # if (keys[pygame.K_DOWN] and y < (720-height)) or (keys[pygame.K_s] and y < (720-height)):
         #     y += speed
-        if keys[pygame.K_SPACE]:
+        if keys[pygame.K_SPACE] or keys[pygame.K_w] or keys[pygame.K_UP]:
             isJump = True
     else:
         if jumpCount >= -10:
@@ -55,8 +76,5 @@ while run:
         else:
             isJump = False
             jumpCount = 10
-    win.fill(black)
-    pygame.draw.rect(win, white, (x, y, width, height))
-    pygame.display.update()
+    draw_window()
 pygame.quit()
-
