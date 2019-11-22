f38fb46e1f05f11eb8816f30c91d43462bc5ba60
DanilaOl <danilaolyanin@yandex.ru>
2019-11-22 21:55:01 +0300

    Sort into folders

--- Game.py
+++ Game.py
@@ -1,27 +1,27 @@
 import pygame
+
 pygame.init()
 
 Game_Name = 'Ванильный питон'
 win = pygame.display.set_mode((1280, 720))
 pygame.display.set_icon(pygame.image.load('Mascot.png'))
 pygame.display.set_caption(Game_Name)
-hero_run_right = [pygame.image.load('Hero run right(1).png'),
-                  pygame.image.load('Hero run right(2).png'),
-                  pygame.image.load('Hero run right(3).png'),
-                  pygame.image.load('Hero run right(4).png')]
+hero_run_right = [pygame.image.load('Hero/Hero run right(1).png'),
+                  pygame.image.load('Hero/Hero run right(2).png'),
+                  pygame.image.load('Hero/Hero run right(3).png'),
+                  pygame.image.load('Hero/Hero run right(4).png')]
 
-hero_run_left = [pygame.image.load('Hero run left(1).png'),
-                 pygame.image.load('Hero run left(2).png'),
-                 pygame.image.load('Hero run left(3).png'),
-                 pygame.image.load('Hero run left(4).png')]
+hero_run_left = [pygame.image.load('Hero/Hero run left(1).png'),
+                 pygame.image.load('Hero/Hero run left(2).png'),
+                 pygame.image.load('Hero/Hero run left(3).png'),
+                 pygame.image.load('Hero/Hero run left(4).png')]
 
-hero_stands_left = [pygame.image.load('Hero left.png'),
-                    pygame.image.load('Hero smoke left.png')]
+hero_stands_left = [pygame.image.load('Hero/Hero stand left.png'),
+                    pygame.image.load('Hero/Hero smoke left.png')]
 
-hero_stands_right = [pygame.image.load('Hero right.png'),
-                    pygame.image.load('Hero smoke.png')]
+hero_stands_right = [pygame.image.load('Hero/Hero stand right.png'),
+                     pygame.image.load('Hero/Hero smoke right.png')]
 
-hero_jump = pygame.image.load('Hero jump.png')
 background = pygame.transform.scale(pygame.image.load('Background.png'), (1280, 720))
 clock = pygame.time.Clock()
 
@@ -31,6 +31,7 @@ speed = 5
 x = 50
 y = 720 - height - 50
 
+game_running = True
 left = False
 right = True
 isRunning = False
@@ -38,6 +39,44 @@ isJump = False
 jumpCount = 10
 animCount = 0
 smokeAnimCount = 0
+
+
+def f_hero_stands_left():
+    global smokeAnimCount
+    if not isJump and not isRunning and left:
+        win.blit(pygame.transform.scale(hero_stands_left[smokeAnimCount // 10], (35, 115)), (x, y))
+        smokeAnimCount += 1
+
+
+def f_hero_stands_right():
+    global smokeAnimCount
+    if not isJump and not isRunning and right:
+        win.blit(pygame.transform.scale(hero_stands_right[smokeAnimCount // 10], (35, 115)), (x, y))
+        smokeAnimCount += 1
+
+
+def f_hero_run_left():
+    global animCount
+    if not isJump and isRunning and left:
+        win.blit(pygame.transform.scale(hero_run_left[animCount // 5], (55, 115)), (x, y))
+        animCount += 1
+
+
+def f_hero_run_right():
+    global animCount
+    if not isJump and isRunning and right:
+        win.blit(pygame.transform.scale(hero_run_right[animCount // 5], (55, 115)), (x, y))
+        animCount += 1
+
+
+def f_hero_jump_left():
+    if isJump and left:
+        win.blit(pygame.transform.scale(pygame.image.load('Hero/Hero jump left.png'), (68, 108)), (x, y))
+
+
+def f_hero_jump_right():
+    if isJump and right:
+        win.blit(pygame.transform.scale(pygame.image.load('Hero/Hero jump right.png'), (68, 108)), (x, y))
 
 
 def game_quit():
@@ -52,33 +91,22 @@ def game_quit():
 
 def draw_window():
     global isRunning
-    global left
-    global right
     global animCount
     global smokeAnimCount
+
+    win.blit(background, (0, 0))
+
     if animCount + 1 >= 20:
         animCount = 0
     if smokeAnimCount + 1 >= 20:
         smokeAnimCount = 0
 
-    win.blit(background, (0, 0))
-
-    if left and not isRunning and not isJump:
-        win.blit(pygame.transform.scale(hero_stands_left[smokeAnimCount // 10], (35, 115)), (x, y))
-        smokeAnimCount += 1
-    if right and not isRunning and not isJump:
-        win.blit(pygame.transform.scale(hero_stands_right[smokeAnimCount // 10], (35, 115)), (x, y))
-        smokeAnimCount += 1
-    if right and isRunning and not isJump:
-        win.blit(pygame.transform.scale(hero_run_right[animCount // 5], (55, 115)), (x, y))
-        animCount += 1
-    if left and isRunning and not isJump:
-        win.blit(pygame.transform.scale(hero_run_left[animCount // 5], (55, 115)), (x, y))
-        animCount += 1
-    if isJump and left:
-        win.blit(pygame.transform.scale(pygame.image.load('Hero jump left.png'), (68, 108)), (x, y))
-    if isJump and right:
-        win.blit(pygame.transform.scale(pygame.image.load('Hero jump.png'), (68, 108)), (x, y))
+    f_hero_stands_left()
+    f_hero_stands_right()
+    f_hero_run_left()
+    f_hero_run_right()
+    f_hero_jump_left()
+    f_hero_jump_right()
 
     for bla in pygame.event.get():
         if bla.type == pygame.KEYUP:
@@ -105,7 +133,6 @@ def jump():
         jumpCount = 10
 
 
-game_running = True
 while game_running:
     clock.tick(120)
     game_quit()
