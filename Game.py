d9d7461008723df0156f5159442222c963fd8e05
DanilaOl <danilaolyanin@yandex.ru>
2019-11-24 17:53:01 +0300

    Minor update

--- Game.py
+++ Game.py
@@ -2,8 +2,16 @@ import pygame
 
 pygame.init()
 
+display_width = 1280
+display_height = 720
+hero_width = 84
+hero_height = 138
+speed = 5
+x = 50
+y = display_height - hero_height - 20
+
 Game_Name = 'Ванильный питон'
-win = pygame.display.set_mode((1280, 720))
+win = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_icon(pygame.image.load('Mascot.png'))
 pygame.display.set_caption(Game_Name)
 hero_run_right = [pygame.image.load('Hero/Hero run right(1).png'),
@@ -22,17 +30,11 @@ hero_stands_left = [pygame.image.load('Hero/Hero stand left.png'),
 hero_stands_right = [pygame.image.load('Hero/Hero stand right.png'),
                      pygame.image.load('Hero/Hero smoke right.png')]
 
-background = pygame.transform.scale(pygame.image.load('Warehouse level.png'), (1280, 720))
+background = pygame.transform.scale(pygame.image.load('Warehouse level.png'), (display_width, display_height))
 pygame.mixer.music.load('Soundtrack.mp3')
 pygame.mixer.music.play(-1)
 pygame.mixer.music.set_volume(0.25)
 clock = pygame.time.Clock()
-
-width = 84
-height = 138
-speed = 5
-x = 50
-y = 720 - height - 20
 
 game_running = True
 left = False
@@ -44,75 +46,83 @@ isSitting = False
 jumpCount = 10
 animCount = 0
 smokeAnimCount = 0
+facing = 1
 
 
 class BulletClass:
-    def __init__(self, x, y, facing):
-        self.x = x
-        self.y = y
-        self.facing = facing
-        self.bullet_speed = 12 * facing
+    def __init__(self, bullet_x, bullet_y, bullet_facing):
+        self.bullet_x = bullet_x
+        self.bullet_y = bullet_y
+        self.bullet_facing = bullet_facing
+        self.bullet_speed = 12 * bullet_facing
 
-    def draw(self, win):
-        win.blit(pygame.transform.scale(pygame.image.load('Bullet GG.png'), (5, 3)), (self.x, self.y))
+    def draw(self, display_name):
+        display_name.blit(pygame.transform.scale(pygame.image.load('Bullet GG.png'), (5, 3)),
+                          (self.bullet_x, self.bullet_y))
 
 
 def f_hero_stands_left():
     global smokeAnimCount
     if not isSitting and not isShooting and not isJump and not isRunning and left:
-        win.blit(pygame.transform.scale(hero_stands_left[smokeAnimCount // 10], (width, height)), (x, y))
+        win.blit(pygame.transform.scale(hero_stands_left[smokeAnimCount // 10], (hero_width, hero_height)), (x, y))
         smokeAnimCount += 1
 
 
 def f_hero_stands_right():
     global smokeAnimCount
     if not isSitting and not isShooting and not isJump and not isRunning and right:
-        win.blit(pygame.transform.scale(hero_stands_right[smokeAnimCount // 10], (width, height)), (x, y))
+        win.blit(pygame.transform.scale(hero_stands_right[smokeAnimCount // 10], (hero_width, hero_height)), (x, y))
         smokeAnimCount += 1
 
 
 def f_hero_run_left():
     global animCount
     if not isSitting and not isShooting and not isJump and isRunning and left:
-        win.blit(pygame.transform.scale(hero_run_left[animCount // 5], (width, height)), (x, y))
+        win.blit(pygame.transform.scale(hero_run_left[animCount // 5], (hero_width, hero_height)), (x, y))
         animCount += 1
 
 
 def f_hero_run_right():
     global animCount
     if not isSitting and not isShooting and not isJump and isRunning and right:
-        win.blit(pygame.transform.scale(hero_run_right[animCount // 5], (width, height)), (x, y))
+        win.blit(pygame.transform.scale(hero_run_right[animCount // 5], (hero_width, hero_height)), (x, y))
         animCount += 1
 
 
 def f_hero_jump_left():
     if not isSitting and not isShooting and isJump and left:
-        win.blit(pygame.transform.scale(pygame.image.load('Hero/Hero jump left.png'), (width, height)), (x, y))
+        win.blit(pygame.transform.scale(pygame.image.load('Hero/Hero jump left.png'),
+                                        (hero_width, hero_height)), (x, y))
 
 
 def f_hero_jump_right():
     if not isSitting and not isShooting and isJump and right:
-        win.blit(pygame.transform.scale(pygame.image.load('Hero/Hero jump right.png'), (width, height)), (x, y))
+        win.blit(pygame.transform.scale(pygame.image.load('Hero/Hero jump right.png'),
+                                        (hero_width, hero_height)), (x, y))
 
 
 def f_hero_shoot_left():
     if not isSitting and isShooting and left:
-        win.blit(pygame.transform.scale(pygame.image.load('Hero/Hero shoot left.png'), (width, height)), (x, y))
+        win.blit(pygame.transform.scale(pygame.image.load('Hero/Hero shoot left.png'),
+                                        (hero_width, hero_height)), (x, y))
 
 
 def f_hero_shoot_right():
     if not isSitting and isShooting and right:
-        win.blit(pygame.transform.scale(pygame.image.load('Hero/Hero shoot right.png'), (width, height)), (x, y))
+        win.blit(pygame.transform.scale(pygame.image.load('Hero/Hero shoot right.png'),
+                                        (hero_width, hero_height)), (x, y))
 
 
 def f_hero_sit_left():
     if isSitting and left:
-        win.blit(pygame.transform.scale(pygame.image.load('Hero/Hero sit left.png'), (width, height)), (x, y))
+        win.blit(pygame.transform.scale(pygame.image.load('Hero/Hero sit left.png'),
+                                        (hero_width, hero_height)), (x, y))
 
 
 def f_hero_sit_right():
     if isSitting and right:
-        win.blit(pygame.transform.scale(pygame.image.load('Hero/Hero sit right.png'), (width, height)), (x, y))
+        win.blit(pygame.transform.scale(pygame.image.load('Hero/Hero sit right.png'),
+                                        (hero_width, hero_height)), (x, y))
 
 
 def draw_window():
@@ -137,8 +147,8 @@ def draw_window():
     f_hero_shoot_right()
     f_hero_sit_left()
     f_hero_sit_right()
-    for bullet in bullets:
-        bullet.draw(win)
+    for i in bullets:
+        i.draw(win)
 
     pygame.display.update()
 
@@ -179,7 +189,7 @@ def key_events():
                     facing = 1
                 if left:
                     facing = -1
-                bullets.append(BulletClass(round(x + width // 2), round(y + height - 92), facing))
+                bullets.append(BulletClass(round(x + hero_width // 2), round(y + hero_height - 92), facing))
             if event.key == pygame.K_DOWN or event.key == pygame.K_s:
                 isSitting = True
 
@@ -201,8 +211,8 @@ while game_running:
     key_events()
 
     for bullet in bullets:
-        if 1280 > bullet.x > 0:
-            bullet.x += bullet.bullet_speed
+        if display_width > bullet.bullet_x > 0:
+            bullet.bullet_x += bullet.bullet_speed
         else:
             bullets.pop(bullets.index(bullet))
 
@@ -214,7 +224,8 @@ while game_running:
             left = True
             right = False
             isRunning = True
-        if (keys[pygame.K_RIGHT] and x < (1280 - width - 5)) or (keys[pygame.K_d] and x < (1280 - width - 5)):
+        if (keys[pygame.K_RIGHT] and x < (display_width - hero_width - 5)) or (keys[pygame.K_d] and
+                                                                               x < (display_width - hero_width - 5)):
             x += speed
             left = False
             right = True
